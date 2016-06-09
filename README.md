46afd85fcaf544fb5b69f4431fda0234d1d0dca2
Jack Baron <yodude107@gmail.com>
2016-06-09 23:51:57 +0100

    New update: New Line thing New readme

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## What?
 Simple, yet easy to use log system for NodeJS
+Made by Jack Baron
+
+Example output:
+`[INFO] [1/1/1970 00:00:00] Log Thing` 
 
 ## How?
 
@@ -10,5 +14,25 @@ Simple, yet easy to use log system for NodeJS
 var log = require("fancylog");
 
 // Use the file
+log.v("Log Message");
+```
+
+---
+
+### What functions can I use?
+```js
+// Info
+log.i("Log Message");
+
+// Debug
+log.d("Log Message");
+
+// Error
+log.e("Log Message");
+
+// Warn
+log.w("Log Message");
+
+// Verbose
 log.v("Log Message");
 ```
--- log.js
+++ log.js
@@ -8,38 +8,50 @@ var createIfNotExist = require("create-if-not-exist");
 var fs = require("fs");
 
 function log(msg, level, color) {
-  // Set Path
-  var logPath = "./" + logName;
-  
-  // Gets the date
-  var d = new Date();
-  // Constructs a date formatted message
-  // "You must constuct additional date formatted messages." - DerpyChap
-  var termData 	= color + '[' + level + ']\x1b[37m [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
-  var logData 	= '[' + level + '] [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
-  
-  // Show this in the console
-  console.log(termData);
-  
-  // Create a log file if it doesn't already exist. Used on first run.
-  createIfNotExist(logPath, "");
-  // Check if it can be written.
-  fs.access(logPath, fs.R_OK | fs.W_OK, function(err) {
-    // Uh oh
-    if (err) {
-      // cant save to file
-      console.error("Failed to output to " + logName);
-    } else {
-      // Try to log
-      fs.appendFile(logPath, logData + '\n', 'utf8', function(err) {
-        // If no worky...
-        if (err) {
-          // Whoopsy
-          console.error("Failed to output to " + logName);
-        }
-      });
-    }
-  });
+  if (msg == null) {
+    // If no msg arg, make a newline.
+    createIfNotExist(logPath, "");
+    fs.appendFile(logPath, "---" + '\n', 'utf8', function(err) {
+      // If no worky...
+      if (err) {
+        // Whoopsy
+        console.error("Failed to output to log");
+      }
+    });
+  } else {
+    // Set Path
+    var logPath = "./" + logName;
+    
+    // Gets the date
+    var d = new Date();
+    // Constructs a date formatted message
+    // "You must constuct additional date formatted messages." - DerpyChap
+    var termData 	= color + '[' + level + ']\x1b[37m [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
+    var logData 	= '[' + level + '] [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
+    
+    // Show this in the console
+    console.log(termData);
+    
+    // Create a log file if it doesn't already exist. Used on first run.
+    createIfNotExist(logPath, "");
+    // Check if it can be written.
+    fs.access(logPath, fs.R_OK | fs.W_OK, function(err) {
+      // Uh oh
+      if (err) {
+        // cant save to file
+        console.error("Failed to output to " + logName);
+      } else {
+        // Try to log
+        fs.appendFile(logPath, logData + '\n', 'utf8', function(err) {
+          // If no worky...
+          if (err) {
+            // Whoopsy
+            console.error("Failed to output to " + logName);
+          }
+        });
+      }
+    });
+  }
 }
 
 function info(msg) {
