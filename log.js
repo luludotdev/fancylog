f8cb3c2a17a80db5df126a2c4579d438d0879a0a
Jack Baron <yodude107@gmail.com>
2016-08-29 01:20:54 +0300

    Cleanup and configurables

--- log.js
+++ log.js
@@ -1,11 +1,26 @@
-// Fancy log file function
-// Made by lolPants
+/*
+  FancyLog - Logging made Pretty
+  By Jack Baron
+  Copyright (c) Jack Baron 2016
+  Licensed under ISC License
+*/
 
-var logName = "default.log"
+// Configurables
+var logName = "default.log";
+var logToFile = true;
 
 // Require Dependants
 var createIfNotExist = require("create-if-not-exist");
 var fs = require("fs");
+
+// Colour Definitions
+var termRed     = `\x1b[31m`;
+var termGreen   = `\x1b[32m`;
+var termYellow  = `\x1b[33m`;
+var termBlue    = `\x1b[34m`;
+var termMagenta = `\x1b[35m`;
+var termCyan    = `\x1b[36m`;
+var termReset   = `\x1b[39m`;
 
 function log(msg, level, color) {
   // Set Path
@@ -14,21 +29,15 @@ function log(msg, level, color) {
   if (msg == null) {
     // If no msg arg, make a newline.
     createIfNotExist(logPath, "");
-    fs.appendFile(logPath, "---" + '\n', 'utf8', function(err) {
-      // If no worky...
-      if (err) {
-        // Whoopsy
-        console.error("Failed to output to log");
-      }
-    });
+    if (logToFile) fs.appendFile(logPath, `---\n`, 'utf8', err => { if (err) console.error("Failed to output to log."); });
   } else {
     
     // Gets the date
     var d = new Date();
     // Constructs a date formatted message
     // "You must constuct additional date formatted messages." - DerpyChap
-    var termData 	= color + '[' + level + ']\x1b[37m [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
-    var logData 	= '[' + level + '] [' + d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '] ' + msg;
+    var termData  = `${color}[${level}] ${termReset}[${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] ${msg}`;
+    var logData 	= `[${level}] [${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] ${msg}`;
     
     // Show this in the console
     console.log(termData);
@@ -36,43 +45,33 @@ function log(msg, level, color) {
     // Create a log file if it doesn't already exist. Used on first run.
     createIfNotExist(logPath, "");
     // Check if it can be written.
-    fs.access(logPath, fs.R_OK | fs.W_OK, function(err) {
+    fs.access(logPath, fs.R_OK | fs.W_OK, err => {
       // Uh oh
       if (err) {
         // cant save to file
         console.error("Failed to output to " + logName);
       } else {
         // Try to log
-        fs.appendFile(logPath, logData + '\n', 'utf8', function(err) {
-          // If no worky...
-          if (err) {
-            // Whoopsy
-            console.error("Failed to output to " + logName);
-          }
-        });
+        if (logToFile) fs.appendFile(logPath, `${logData}\n`, 'utf8', err => { if (err) console.error("Failed to output to log."); });
       }
     });
   }
 }
 
 function info(msg) {
-  log(msg, "INFO", "\x1b[32m");
+  log(msg, "INFO", termGreen);
 }
-
 function debug(msg) {
-  log(msg, "DEBUG", "\x1b[33m");
+  log(msg, "DEBUG", termCyan);
 }
-
 function error(msg) {
-  log(msg, "ERROR", "\x1b[31m");
+  log(msg, "ERROR", termRed);
 }
-
 function verbose(msg) {
-  log(msg, "VERBOSE", "\x1b[39m");
+  log(msg, "VERBOSE", termMagenta);
 }
-
 function warn(msg) {
-  log(msg, "WARN", "\x1b[33m");
+  log(msg, "WARN", termYellow);
 }
 
 module.exports = {
---
+++ tests.js
@@ -0,0 +1,7 @@
+var log = require('./log.js');
+
+log.i('test');
+log.d('test');
+log.e('test');
+log.v('test');
+log.w('test');
