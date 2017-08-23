6f621143682c7eb370fe2b5a8b804c49f0e10c2d
Jack Baron <yodude107@gmail.com>
2017-08-23 09:40:50 +0100

    Start Backend Rewrite

--- index.js
+++
@@ -1,8 +0,0 @@
-/*
-  FancyLog - Logging made Pretty
-  By Jack Baron
-  Copyright (c) Jack Baron 2016
-  Licensed under ISC License
-*/
-
-module.exports = require(require('path').join(__dirname, 'src'))
